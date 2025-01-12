fd0eb9668e744ddeee17381179e7fcb91ddf6d82
Ravan <ravan2003@gmail.com>
2025-01-12 13:10:10 +0800

    Adjusted colors and added logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import LinkedButton from "./components/Button";
 import chinese_menu from "./assets/chinese_menu.pdf";
 import english_menu from "./assets/english_menu.pdf";
+import logo from "./assets/spread_logo.png";
 
 function App() {
   return (
-    <div className="h-screen w-screen bg-slate-400 flex flex-col text-center p-16">
-      Welcome to the Spread!
+    <div className="h-screen w-screen bg-[#181a26] flex flex-col text-center p-16 text-slate-200 items-center">
+      <img src={logo} alt="logo" className="w-[480px]" />
       <div className="flex flex-col border-1 border-red-500 justify-center gap-4 m-12">
         <LinkedButton text={"English Menu"} link={english_menu} />
         <LinkedButton text={"Chinese Menu / 中文菜单"} link={chinese_menu} />
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,9 @@
 export default function LinkedButton({ text, link }) {
   return (
     <a href={link} className="hover:cursor-pointer flex justify-center">
-      <div className="border-2 border-black p-4 rounded-xl w-64">{text}</div>
+      <div className="border-2 border-black p-4 rounded-xl w-64 bg-[#f0ece4] text-black">
+        {text}
+      </div>
     </a>
   );
 }
